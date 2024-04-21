3a0d705ee44edece419d7f6f3ead2de2d8ca342a
Ape Gainz <akshit360@live.com>
2024-04-21 14:59:39 +0400

    feat: add wagmi and rainbowkit providers and create env example file

---
+++ src/app/providers.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import * as React from "react";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { base, sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+const { wallets } = getDefaultWallets();
+
+export const config = getDefaultConfig({
+  appName: "DM hub app",
+  projectId: process.env.NEXT_PUBLIC_RAINBOW_KIT_ID ?? "API_KEY",
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [sepolia, base],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
