e52212182e6be52682770358213e0ebc8cf5ea0c
Angad Singh <angad.singh@imedia24.de>
2024-04-23 19:37:36 +0100

    Wrap CreateBetSuccess in a Suspense boundary

--- src/app/create-bet/success/page.tsx
+++ src/app/create-bet/success/page.tsx
@@ -1,95 +1,13 @@
 "use client";
 
-import PixelatedHeadingContainer from "@/app/components/PixelatedHeadingContainer";
-import { useReadContract } from "wagmi";
-import { DEGEN_MARKETS_ABI } from "@/app/lib/utils/bets/abis";
-import { DEGEN_MARKETS_ADDRESS } from "@/app/lib/utils/bets/constants";
-import { useSearchParams } from "next/navigation";
-import BetCountdown from "@/app/components/BetCoundown";
+import React, { Suspense } from "react";
+import CreateBetSuccess from "@/app/create-bet/success/CreateBetSuccess";
 
-const CreateBetSuccess = () => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
-  const { data }: { data?: any[] } = useReadContract({
-    abi: DEGEN_MARKETS_ABI,
-    address: DEGEN_MARKETS_ADDRESS,
-    functionName: "betIdToBet",
-    args: [id],
-  });
-  const creationTimestamp = data ? data[2] : 0;
-  const ticker = data ? data[3] : "";
-  const metric = data ? data[4] : "";
-  const direction = data ? (data[6] === true ? "up" : "down") : "";
-
-  const handleShare = () => {
-    if (navigator.share) {
-      const url = `${window.location.protocol}//${window.location.hostname}/bets/${id}`;
-
-      navigator
-        .share({
-          title: "Check out my bet!",
-          text: "I just made a bet! Check it out:",
-          url: url,
-        })
-        .catch((error) => {
-          console.error("Error sharing:", error);
-        });
-    } else {
-      console.log("Web Share API is not supported in your browser.");
-    }
-  };
-
-  const handleCopy = async () => {
-    const url = `${window.location.protocol}//${window.location.hostname}/bets/${id}`;
-
-    try {
-      await navigator.clipboard.writeText(url);
-    } catch (err) {
-      console.error("Failed to copy URL: ", err);
-    }
-  };
-
+const Page = () => {
   return (
-    <main className="text-center">
-      <div className="flex justify-center">
-        <PixelatedHeadingContainer classNames="my-10 w-[500px]">
-          Bet Created!
-        </PixelatedHeadingContainer>
-      </div>
-      <div className="flex justify-center">
-        <BetCountdown
-          classNames="bg-blue-dark w-max p-2 border-2 border-yellow-dark"
-          betCreationTimestamp={creationTimestamp}
-          duration={60 * 60 * 4 * 1000}
-        />
-      </div>
-      <br />
-      <br />
-      <br />
-      <div className="text-blue-dark">
-        Your bet on {ticker}&apos;s {metric} going {direction} was successfully
-        created!
-        <br />
-        Challenge your frens by giving them a link to this bet. They have 4
-        hours to accept!
-      </div>
-      <br />
-      <br />
-      <div className="flex justify-center gap-[60px]">
-        <button
-          className="text-blue-dark bg-yellow-dark px-3 py-1 border-2 border-blue-dark"
-          onClick={handleShare}
-        >
-          Share
-        </button>
-        <button
-          className="text-blue-dark bg-yellow-dark px-3 py-1 border-2 border-blue-dark"
-          onClick={handleCopy}
-        >
-          Copy
-        </button>
-      </div>
-    </main>
+    <Suspense>
+      <CreateBetSuccess />
+    </Suspense>
   );
 };
-export default CreateBetSuccess;
+export default Page;
