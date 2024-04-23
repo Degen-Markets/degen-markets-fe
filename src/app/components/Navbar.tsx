ae3fd2e62caee12f4265fddb24fd8a038d500735
Ape Gainz <akshit360@live.com>
2024-04-23 21:51:13 +0400

    fix: navbar logo anredirects to homepage

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,16 +7,20 @@ const Navbar: React.FC<{}> = ({}) => {
   return (
     <div className="w-full flex items-center">
       <div className="flex justify-between w-full px-10 py-5 items-center">
-        <div className="flex gap-x-2 items-center">
-          <Image
-            src="/dm-logo.png"
-            alt={"DB"}
-            width={100}
-            height={100}
-            className="w-12 h-12"
-          />
-          <div className="uppercase text-4xl tracking-wide">Degen markets</div>
-        </div>
+        <Link href={"/"}>
+          <div className="flex gap-x-2 items-center">
+            <Image
+              src="/dm-logo.png"
+              alt={"DB"}
+              width={100}
+              height={100}
+              className="w-12 h-12"
+            />
+            <div className="uppercase text-4xl tracking-wide">
+              Degen markets
+            </div>
+          </div>
+        </Link>
         <div className="flex gap-4 items-center">
           <Link href={"/create-bet"}>
             <div className="bg-neutral-950 text-neutral-200 rounded-md px-4 py-2">
