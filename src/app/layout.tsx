6a8343fd364b71ccc57c8d5634d094c28a733370
Angad Singh <angad.singh@imedia24.de>
2024-04-23 19:54:27 +0100

    Use font-family in css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "./providers";
 
-const font = localFont({ src: "./Jersey10-regular.ttf" });
-
 export const metadata: Metadata = {
   title: "Degen Markets",
   description: "Degen Markets Hub",
@@ -18,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${font.className} text-2xl bg-cover bg-center bg-no-repeat min-h-screen`}
+        className="text-2xl bg-cover bg-center bg-no-repeat min-h-screen"
         style={{ backgroundImage: "url('/bet-bg.svg')" }}
       >
         <Providers>{children}</Providers>
