03c82ec311087c1535337815c8ebf86ed4382131
Angad Singh <angad.singh@imedia24.de>
2024-04-24 10:53:17 +0100

    use dev BE url

--- src/app/lib/utils/api/getBets.ts
+++ src/app/lib/utils/api/getBets.ts
@@ -2,4 +2,4 @@ import axios, { AxiosResponse } from "axios";
 import { BetsResponse } from "@/app/lib/utils/bets/types";
 
 export const getBets = (): Promise<AxiosResponse<BetsResponse>> =>
-  axios.get("http://bets-backend-1257565388.eu-west-1.elb.amazonaws.com/bets");
+  axios.get("http://ec2-52-49-142-79.eu-west-1.compute.amazonaws.com/bets");
