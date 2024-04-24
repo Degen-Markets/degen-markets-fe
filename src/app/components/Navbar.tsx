14f3b2997fd0e0bed5796b6ebe630d8baea11080
Angad Singh <angad.singh@imedia24.de>
2024-04-24 11:24:10 +0100

    Make navbar buttons transparent

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -9,37 +9,20 @@ const Navbar: React.FC<{}> = ({}) => {
       <div className="flex justify-between w-full px-10 py-5 items-center">
         <Link href={"/"}>
           <div className="flex gap-x-2 items-center">
-            <Image
-              src="/dm-logo.png"
-              alt={"DB"}
-              width={100}
-              height={100}
-              className="w-12 h-12"
-            />
-            <div className="uppercase text-4xl tracking-wide">
+            <div className="uppercase text-8xl tracking-wide">
               Degen markets
             </div>
           </div>
         </Link>
         <div className="flex gap-4 items-center">
-          <Link href="/bets">
-            <button className="flex flex-row masked-button p-1 rounded-full text-3xl w-fit cursor-pointer">
-              <span className="flex flex-row bg-blue-dark rounded-full px-2 py-0.5">
-                <span className="masked-button-text flex geo-font cursor-pointer">
-                  existing bets
-                  <span className="gradient-button-arrow flex items-center"></span>
-                </span>
-              </span>
-            </button>
-          </Link>
+          {/*<Link href="/bets">*/}
+          {/*  <button className="py-0.5 px-1 rounded-full text-3xl text-yellow-light w-fit cursor-pointer border-yellow-light border-4">*/}
+          {/*    existing bets*/}
+          {/*  </button>*/}
+          {/*</Link>*/}
           <Link href="/create-bet">
-            <button className="flex flex-row masked-button p-1 rounded-full text-3xl w-fit cursor-pointer">
-              <span className="flex flex-row bg-blue-dark rounded-full px-2 py-0.5">
-                <span className="masked-button-text flex geo-font cursor-pointer">
-                  create bet
-                  <span className="gradient-button-arrow flex items-center"></span>
-                </span>
-              </span>
+            <button className="py-0.5 px-1 rounded-full text-3xl text-pink-light w-fit cursor-pointer border-pink-light border-4">
+              create bet
             </button>
           </Link>
           <div className="text-2xl">
