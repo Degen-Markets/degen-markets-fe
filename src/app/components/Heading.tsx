8fc55cb89b72f904d58ddc6b26ad2a0a99b2efeb
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-04-25 23:19:02 +0100

    Optimize rendering of 8-bit pixel art border

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -13,9 +13,9 @@ const Headline: FC<{ className?: string } & PropsWithChildren> = ({
   className,
 }) => {
   return (
-    <div className={cx("relative bg-blue-dark text-center", className)}>
+    <div className={cx("relative", className)}>
       <div
-        className={`bg-blue-dark py-12 inline-block text-8xl px-6 after:absolute after:bg-inherit after:h-[80%] after:w-4 after:-right-4 after:top-[50%] after:translate-y-[-50%] before:absolute before:bg-inherit before:h-[80%] before:w-4 before:-left-4 before:top-[50%] before:translate-y-[-50%]`}
+        className={`eight-bit-border-20 bg-blue-dark py-8 text-8xl px-6 text-center`}
       >
         {children}
       </div>
@@ -32,7 +32,7 @@ const SubHeadline: FC<
         "bg-blue-dark  p-2 border-2 border-pink-light inline-block mt-8 ",
         className,
         {
-          "absolute mt-0 left-[50%] -translate-x-[50%] -top-[50%] translate-y-[70%] ":
+          "absolute mt-0 left-[50%] -translate-x-[50%] -top-[50%] translate-y-[calc(50%-10px)]":
             isTop,
         },
       )}
