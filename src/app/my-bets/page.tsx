7651f1568ec9c6a847d193b50a9208c2ef179120
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-04-28 15:35:37 +0100

    Filter by open and closed bet

--- src/app/my-bets/page.tsx
+++ src/app/my-bets/page.tsx
@@ -4,20 +4,41 @@ import { getBets } from "@/app/lib/utils/api/getBets";
 import { useAccount } from "wagmi";
 import { useEffect, useState } from "react";
 import { BetsResponse } from "@/app/lib/utils/bets/types";
+import { zeroAddress } from "viem";
+import { DEFAULT_BET_DURATION } from "@/app/lib/utils/bets/constants";
 
 const MyBets = () => {
   const { address } = useAccount();
-  const [bets, setBets] = useState<BetsResponse>([]);
+  const [openBets, setOpenBets] = useState<BetsResponse>([]);
+  const [closedBets, setClosedBets] = useState<BetsResponse>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchBetsByAddress = async (address: `0x${string}` | undefined) => {
     try {
-      const { data: betsResponse } = await getBets();
-      const betForGivenAddress = betsResponse.filter(
-        (bet) => bet.creator === address,
+      setIsLoading(true);
+      const { data: fetchedBets } = await getBets();
+      const currentTime = Date.now();
+
+      const userBets = fetchedBets.filter((bet) => bet.creator === address);
+      const userOpenBets = userBets.filter(
+        (bet) =>
+          (bet.acceptor === null || bet.acceptor === zeroAddress) &&
+          parseInt(bet.creationTimestamp) * 1000 + DEFAULT_BET_DURATION >
+            currentTime,
       );
-      setBets(betForGivenAddress);
+
+      const userClosedBets = userBets.filter(
+        (bet) =>
+          parseInt(bet.creationTimestamp) * 1000 + DEFAULT_BET_DURATION <=
+          currentTime,
+      );
+
+      setOpenBets(userOpenBets);
+      setClosedBets(userClosedBets);
     } catch (error) {
       console.error("Error fetching bets:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -37,16 +58,25 @@ const MyBets = () => {
 
   return (
     <div>
-      {bets.length > 0 && (
-        <>
-          <div className="text-center text-7xl">Your Bets:</div>
-          <div className="p-5 flex flex-wrap gap-[40px] justify-center">
-            {bets.map((bet) => (
-              <BetCard key={bet.id} bet={bet} />
-            ))}
-          </div>
-        </>
+      {isLoading && <div className="text-center text-4xl">Loading...</div>}
+
+      {openBets.length > 0 && (
+        <div className="text-center text-7xl">Open Bets:</div>
       )}
+      <div className="p-5 flex flex-wrap gap-[40px] justify-center">
+        {openBets.map((bet) => (
+          <BetCard key={bet.id} bet={bet} />
+        ))}
+      </div>
+
+      {closedBets.length > 0 && (
+        <div className="text-center text-7xl">Closed Bets:</div>
+      )}
+      <div className="p-5 flex flex-wrap gap-[40px] justify-center">
+        {closedBets.map((bet) => (
+          <BetCard key={bet.id} bet={bet} />
+        ))}
+      </div>
     </div>
   );
 };
