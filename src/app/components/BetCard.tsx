b480cacfc08ef8d60f24524797b3d325007b9049
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-04-30 14:37:12 +0100

    [UPD] Use new endpoint to get bets by creator

--- src/app/components/BetCard.tsx
+++ src/app/components/BetCard.tsx
@@ -10,6 +10,7 @@ import { prettifyAddress } from "@/app/lib/utils/evm";
 import { getHumanFriendlyMetric } from "@/app/lib/utils/bets/helpers";
 import { BetResponse } from "@/app/lib/utils/bets/types";
 import useToast from "@/app/components/Toast/useToast";
+import { ButtonPrimary } from "@/app/components/Button";
 
 const BetCard = ({ bet }: { bet: BetResponse }) => {
   const { showToast } = useToast();
@@ -52,27 +53,14 @@ const BetCard = ({ bet }: { bet: BetResponse }) => {
   const CalToAction = () => (
     <>
       {showWithdrawButton ? (
-        <button
-          className="flex flex-row masked-button p-1 rounded-full text-3xl w-fit cursor-pointer"
-          onClick={onWithdraw}
-        >
-          <span className="flex flex-row bg-blue-dark rounded-full px-2 py-0.5">
-            <span className="masked-button-text flex geo-font cursor-pointer">
-              Withdraw
-              <span className="gradient-button-arrow flex items-center"></span>
-            </span>
-          </span>
-        </button>
+        <ButtonPrimary size="regular" onClick={onWithdraw}>
+          Withdraw
+        </ButtonPrimary>
       ) : (
         <Link href={`/bets/${bet.id}`}>
-          <button className="flex flex-row masked-button p-1 rounded-full text-3xl w-fit cursor-pointer">
-            <span className="flex flex-row bg-blue-dark rounded-full px-2 py-0.5">
-              <span className="masked-button-text flex geo-font cursor-pointer">
-                {isBetExpired ? "View details" : "Accept bet"}
-                <span className="gradient-button-arrow flex items-center"></span>
-              </span>
-            </span>
-          </button>
+          <ButtonPrimary size="regular">
+            {isBetExpired ? "View details" : "Accept bet"}
+          </ButtonPrimary>
         </Link>
       )}
     </>
@@ -83,10 +71,10 @@ const BetCard = ({ bet }: { bet: BetResponse }) => {
       <div className="bg-blue-medium text-blue-dark my-2 p-1">
         {prettifyAddress(bet.creator)} is betting that...
       </div>
-      <div className="bg-white text-blue-dark p-1">
+      <div className="bg-white text-blue-dark p-1 h-[72px]">
         {bet.ticker}&apos;s {getHumanFriendlyMetric(bet.metric)} will go&nbsp;
         {bet.isBetOnUp ? "up" : "down"} in{" "}
-        {parseInt(bet.duration) / 60 / 60 / 24} days.
+        {parseInt(bet.expirationTimestamp) / 60 / 60 / 24} days.
       </div>
 
       <div className="flex justify-center mt-4">
--- src/app/my-bets/page.tsx
+++ src/app/my-bets/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import BetCard from "@/app/components/BetCard";
-import { getBets } from "@/app/lib/utils/api/getBets";
 import { useAccount } from "wagmi";
 import { useEffect, useState } from "react";
 import { BetsResponse } from "@/app/lib/utils/bets/types";
 import { zeroAddress } from "viem";
 import { DEFAULT_BET_DURATION } from "@/app/lib/utils/bets/constants";
 import useToast from "@/app/components/Toast/useToast";
+import { getBetsByCreator } from "@/app/lib/utils/api/getBetsByCreator";
 
 const MyBets = () => {
   const { address, isConnected } = useAccount();
@@ -15,22 +15,23 @@ const MyBets = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const { ToastComponent } = useToast();
 
-  const fetchBetsByAddress = async (address: `0x${string}` | undefined) => {
+  const fetchBetsByAddress = async (address: `0x${string}`) => {
     try {
       setIsLoading(true);
-      const { data: fetchedBets } = await getBets();
       const currentTime = Date.now();
-      const userBets = fetchedBets.filter((bet) => bet.creator === address);
-      const userOpenBets = userBets.filter(
+      const { data: bets } = await getBetsByCreator(address);
+      const userOpenBets = bets.filter(
         (bet) =>
           (bet.acceptor === null || bet.acceptor === zeroAddress) &&
+          !bet.isWithdrawn &&
           parseInt(bet.creationTimestamp) * 1000 + DEFAULT_BET_DURATION >
             currentTime,
       );
-      const userClosedBets = userBets.filter(
+      const userClosedBets = bets.filter(
         (bet) =>
+          bet.isWithdrawn ||
           parseInt(bet.creationTimestamp) * 1000 + DEFAULT_BET_DURATION <=
-          currentTime,
+            currentTime,
       );
       setOpenBets(userOpenBets);
       setClosedBets(userClosedBets);
@@ -58,7 +59,11 @@ const MyBets = () => {
   return (
     <>
       {isLoading && <div className="text-center text-4xl">Loading...</div>}
-
+      {openBets.length === 0 && closedBets.length === 0 && (
+        <div className="text-center text-4xl">
+          You currently do not have any bets.
+        </div>
+      )}
       {openBets.length > 0 && (
         <div className="text-center text-7xl">Open Bets:</div>
       )}
