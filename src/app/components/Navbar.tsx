c3cdc509c9fe007a26f095ccb5b1e2727513c519
Angad Singh <angad.singh@imedia24.de>
2024-05-06 21:44:42 +0530

    Add link to /my-bets

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import { ButtonSecondary, CustomConnectButton } from "@/app/components/Button";
 
-const Navbar: React.FC<{}> = ({}) => {
+const Navbar: React.FC = ({}) => {
   return (
     <div className="w-full flex items-center">
       <div className="flex justify-between w-full px-10 py-5 items-center">
-        <Link href={"/"}>
+        <Link href="/">
           <div className="flex gap-x-2 items-center">
             <div className="uppercase text-8xl tracking-wide">
               Degen markets
@@ -15,10 +15,13 @@ const Navbar: React.FC<{}> = ({}) => {
         </Link>
         <div className="flex justify-center gap-5 items-center ">
           <Link href="/bets">
-            <ButtonSecondary size={"small"}>Existing bets</ButtonSecondary>
+            <ButtonSecondary size="small">Existing bets</ButtonSecondary>
           </Link>
           <Link href="/create-bet">
-            <ButtonSecondary size={"small"}>Create bet</ButtonSecondary>
+            <ButtonSecondary size="small">Create bet</ButtonSecondary>
+          </Link>
+          <Link href="/my-bets">
+            <ButtonSecondary size="small">My bets</ButtonSecondary>
           </Link>
           <CustomConnectButton />
         </div>
