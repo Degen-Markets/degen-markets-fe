7c5b5eb99842f825efa0189ecd8f10dc5d007983
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-05-14 21:40:09 +0100

    debug build error

--- src/app/create-bet/page.tsx
+++ src/app/create-bet/page.tsx
@@ -1,4 +1,4 @@
-import { Heading, Headline } from "@/app/components/Heading";
+import { Heading, Headline, SubHeadline } from "@/app/components/Heading";
 import EthPrice from "@/app/create-bet/_components/EthPrice";
 import SlotMachine from "@/app/create-bet/_components/SlotMachine";
 import ActionButton from "@/app/create-bet/_components/ActionButton";
@@ -8,7 +8,8 @@ const CreateBetPage = () => {
     <main className="text-center">
       <div className="flex justify-center select-none">
         <Heading className="mt-10 mb-12">
-          <Headline>Challenge a fren</Headline>
+          <Headline>Challenge a friend</Headline>
+          <SubHeadline>Test Build error</SubHeadline>
         </Heading>
       </div>
       <div className="flex justify-center select-none">
