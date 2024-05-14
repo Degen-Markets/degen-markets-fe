4a5540ee315e930da973ccf1fd901bc126b4dab0
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-05-14 20:11:45 +0100

    Temp solution to fix build issue

--- src/app/create-bet/page.tsx
+++ src/app/create-bet/page.tsx
@@ -2,24 +2,21 @@ import { Heading, Headline } from "@/app/components/Heading";
 import EthPrice from "@/app/create-bet/_components/EthPrice";
 import SlotMachine from "@/app/create-bet/_components/SlotMachine";
 import ActionButton from "@/app/create-bet/_components/ActionButton";
-import { Suspense } from "react";
 
 const CreateBetPage = () => {
   return (
-    <Suspense>
-      <main className="text-center">
-        <div className="w-[80%] md:w-1/2 mx-auto">
-          <Heading className="mt-10 mb-16 md:mb-20">
-            <Headline>Challenge a fren</Headline>
-          </Heading>
-          <div className="flex justify-center select-none">
-            <SlotMachine />
-          </div>
-          <EthPrice />
-          <ActionButton />
+    <main className="text-center">
+      <div className="w-[80%] md:w-1/2 mx-auto">
+        <Heading className="mt-10 mb-16 md:mb-20">
+          <Headline>Challenge a fren</Headline>
+        </Heading>
+        <div className="flex justify-center select-none">
+          <SlotMachine />
         </div>
-      </main>
-    </Suspense>
+        <EthPrice />
+        <ActionButton />
+      </div>
+    </main>
   );
 };
 
