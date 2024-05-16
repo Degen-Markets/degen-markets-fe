d8a6edcaa424ca5ee6cd0e037549b4aded705f7e
Angad Singh <angad.singh@imedia24.de>
2024-05-16 17:16:11 +0530

    Re-Add Suspense component

--- src/app/create-bet/page.tsx
+++ src/app/create-bet/page.tsx
@@ -2,21 +2,24 @@ import { Heading, Headline } from "@/app/components/Heading";
 import EthPrice from "@/app/create-bet/_components/EthPrice";
 import SlotMachine from "@/app/create-bet/_components/SlotMachine";
 import ActionButton from "@/app/create-bet/_components/ActionButton";
+import { Suspense } from "react";
 
 const CreateBetPage = () => {
   return (
-    <main className="text-center">
-      <div className="w-[80%] md:w-1/2 mx-auto">
-        <Heading className="mt-10 mb-16 md:mb-20">
-          <Headline>Challenge a fren</Headline>
-        </Heading>
-        <div className="flex justify-center select-none">
-          <SlotMachine />
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="text-center">
+        <div className="w-[80%] md:w-1/2 mx-auto">
+          <Heading className="mt-10 mb-16 md:mb-20">
+            <Headline>Challenge a fren</Headline>
+          </Heading>
+          <div className="flex justify-center select-none">
+            <SlotMachine />
+          </div>
+          <EthPrice />
+          <ActionButton />
         </div>
-        <EthPrice />
-        <ActionButton />
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 };
 
