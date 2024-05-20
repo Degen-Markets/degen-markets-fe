e311c038bd5bda6f82614f994c08421d50ee8fe8
Angad Singh <angad17@hotmail.com>
2024-05-20 17:40:11 +0530

    display connected wallet in bets/:id/success if acceptor is null

--- src/app/bets/[id]/success/page.tsx
+++ src/app/bets/[id]/success/page.tsx
@@ -5,9 +5,11 @@ import { useEffect, useState } from "react";
 import { BetResponse } from "@/app/lib/utils/bets/types";
 import { getBetById } from "@/app/lib/utils/api/getBetById";
 import Wrapper from "@/app/components/Wrapper";
+import { useAccount } from "wagmi";
 
 const AcceptBetSuccess = () => {
   const { id } = useParams<{ id?: string }>();
+  const { address } = useAccount();
 
   const [bet, setBet] = useState<BetResponse | null>(null);
 
@@ -30,7 +32,12 @@ const AcceptBetSuccess = () => {
   return (
     <main className="text-center">
       <Wrapper className="lg:max-w-screen-md">
-        <AcceptedBet bet={bet} />
+        <AcceptedBet
+          bet={{
+            ...bet,
+            acceptor: bet.acceptor || address || null, // use connected wallet if acceptor is null
+          }}
+        />
       </Wrapper>
     </main>
   );
