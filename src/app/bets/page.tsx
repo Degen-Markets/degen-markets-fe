ed48bdc9ef0d0279a4fe659ca7f127fb855a1a90
Angad Singh <angad17@hotmail.com>
2024-05-27 21:03:49 +0530

    Small comment for force-dynamic

--- src/app/bets/page.tsx
+++ src/app/bets/page.tsx
@@ -2,7 +2,7 @@ import RecentActivity from "@/app/bets/_components/RecentActivity";
 import BetsTab from "@/app/components/BetsTab";
 import Wrapper from "@/app/components/Wrapper";
 import { getBets } from "@/app/lib/utils/api/getBets";
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // forces Next to not cache api requests
 const Bets = async () => {
   const { data: fetchedBets } = await getBets();
   return (
