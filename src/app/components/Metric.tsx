a421bd2fc692010757407cf934cac26b3f0d417c
Angad Singh <angad17@hotmail.com>
2024-06-08 19:57:33 +0100

    Display seconds, minutes & hours in recent activity

--- src/app/components/Metric.tsx
+++ src/app/components/Metric.tsx
@@ -1,5 +1,8 @@
 import { ArrowDown, ArrowUp } from "@/app/components/Icons";
-import { betDuration } from "@/app/lib/utils/bets/helpers";
+import {
+  betDuration,
+  getHumanFriendlyMetric,
+} from "@/app/lib/utils/bets/helpers";
 import React from "react";
 import { Metric } from "@/app/lib/utils/bets/types";
 
@@ -22,7 +25,7 @@ const MetricDisplay = ({
     <span
       className={`${isBetOnUp ? "text-green-500" : "text-red-500"} flex items-center`}
     >
-      {metric === Metric.MARKET_CAP_DOMINANCE ? "Mrkt Cap Dom" : metric}
+      {getHumanFriendlyMetric(metric)}
       {isBetOnUp ? <ArrowUp /> : <ArrowDown />}
       <span
         className={`text-neutral-800 pl-1 ${className}`}
