d854d6788fe98980358a2053b153104dbe4ec4f2
Angad Singh <angad17@hotmail.com>
2024-06-12 18:17:44 +0100

    Use hardcoded px values to place bull, bear & clouds in create-bet

--- src/app/layouts/BetLayout.tsx
+++ src/app/layouts/BetLayout.tsx
@@ -8,16 +8,16 @@ const BetLayout: FC<{ className?: string } & PropsWithChildren> = ({
 }) => {
   return (
     <main className="w-full">
-      <div className="absolute top-24 right-0 -z-[1] hidden sm:block">
+      <div className="absolute top-[200px] right-0 -z-[1] hidden sm:block">
         <Image src="/bull.png" width={300} height={300} alt="Bull" />
       </div>
-      <div className="absolute top-20 left-0 -z-[1] hidden sm:block">
+      <div className="absolute top-[200px] left-0 -z-[1] hidden sm:block">
         <Image src="/left-cloud.png" width={574} height={208} alt="Bull" />
       </div>
-      <div className="absolute -bottom-10 right-0 -z-[1] hidden sm:block">
+      <div className="absolute top-[700px] right-0 -z-[1] hidden sm:block">
         <Image src="/right-cloud.png" width={346} height={198} alt="Bull" />
       </div>
-      <div className="absolute -bottom-40 left-0 -z-[1] hidden sm:block">
+      <div className="absolute top-[700px] left-0 -z-[1] hidden sm:block">
         <Image src="/bear.png" width={300} height={300} alt="Bear" />
       </div>
       <Wrapper
