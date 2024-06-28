9b11fb3591114c5887aedd6c683eefff77c3509e
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-06-28 13:30:40 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Games from "@/app/home/_sections/Games";
 const Home = () => {
   return (
     <div className="flex flex-col text-white">
+      <h1>TEST</h1>
       <Hero />
       <Games />
       <FellowDegens />
