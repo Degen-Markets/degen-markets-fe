fcc61623c07e816189c1e55fd58df8aed39f0d41
Angad Singh <angad17@hotmail.com>
2024-08-13 00:20:40 +0100

    Mobile optimization for pools

--- src/app/pools/[id]/_components/OptionsTable.tsx
+++ src/app/pools/[id]/_components/OptionsTable.tsx
@@ -2,7 +2,7 @@
 import { Entry, Pool } from "@/app/lib/utils/bets/types";
 import { useAnchorWallet, useWallet } from "@solana/wallet-adapter-react";
 import Table from "@/app/components/Table/Table";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getEntry } from "@/app/lib/utils/api/getEntry";
 import { Button } from "@/app/components/Button";
 import { LAMPORTS_PER_SOL, Transaction } from "@solana/web3.js";
@@ -48,6 +48,8 @@ const OptionsTable = ({ pool }: { pool: Pool }) => {
     );
   }
 
+  console.log(entries);
+
   const data = pool.options.map((option, index) => ({
     optionTitle: option.title,
     stake: (
@@ -81,11 +83,49 @@ const OptionsTable = ({ pool }: { pool: Pool }) => {
   }));
 
   return (
-    <div className="flex flex-col justify-center items-center w-full">
-      <div>{pool.title}</div>
-      <br />
-      <Table columns={columns} data={data} isExpandable={false} />
-    </div>
+    <>
+      <div className="grid gap-4 md:hidden m-5 p-2">
+        {pool.options.map((option, index) => (
+          <div className="flex gap-4 bg-cadet-blue-dark p-1" key={option.id}>
+            <div>{option.title}</div>
+            <div>
+              Your Stake:{" "}
+              {entries[index]?.value
+                ? entries[index]?.value / LAMPORTS_PER_SOL
+                : 0}{" "}
+              SOL
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <Button
+                size="small"
+                onClick={async (event) => {
+                  event.preventDefault();
+                  const entrant = anchorWallet?.publicKey;
+                  if (entrant) {
+                    const { data } = await claimWin(
+                      pool.id,
+                      option.id,
+                      entrant.toString(),
+                    );
+                    const transaction = Transaction.from(
+                      Buffer.from(data.signature, "base64"),
+                    );
+                    await anchorWallet?.signTransaction(transaction);
+                  }
+                }}
+              >
+                Claim
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="hidden md:flex flex-col justify-center items-center w-full">
+        <div>{pool.title}</div>
+        <br />
+        <Table columns={columns} data={data} isExpandable={false} />
+      </div>
+    </>
   );
 };
 
