f32cd11962de703c26034f97bcd254e2324638e3
Angad Singh <angad17@hotmail.com>
2024-08-12 23:17:56 +0100

    remove unused imports

--- src/app/pools/[id]/page.tsx
+++ src/app/pools/[id]/page.tsx
@@ -1,7 +1,4 @@
-import * as anchor from "@coral-xyz/anchor";
 import { getPools } from "@/app/lib/utils/api/getPools";
-import Table from "@/app/components/Table/Table";
-import { useWallet } from "@solana/wallet-adapter-react";
 import OptionsTable from "@/app/pools/[id]/_components/OptionsTable";
 
 const Pool = async ({ params: { id } }: { params: { id: string } }) => {
