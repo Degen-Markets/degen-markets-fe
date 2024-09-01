ab69b4a156ed9b409777b3c20b3fc5429eb6b72e
Qalb E Habib <73225312+QalbeHabib@users.noreply.github.com>
2024-09-01 20:06:13 +0500

    Update src/app/components/Wrapper.tsx
    Co-authored-by: Abdel ahzab <abdou.ahzab@gmail.com>

--- src/app/components/Wrapper.tsx
+++ src/app/components/Wrapper.tsx
@@ -9,7 +9,7 @@ interface Props extends PropsWithChildren {
 const Wrapper = ({ children, className, isHome = false }: Props) => {
   return (
     <div
-      className={cx(
+      className={twMerge(
         "md:mx-auto md:max-w-screen-xl lg:max-w-screen-2xl px-4 lg:px-6",
         {
           "my-20 bg-gradient-to-t from-purple-light to-black-medium pb-40":
