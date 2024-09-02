d34924753424f6bf5b22b83c227cc6195cdf721c
Angad Singh <angadsingh@Angads-Air.Home>
2024-09-02 10:07:45 +0100

    Remove PoolAccount from types

--- src/app/lib/utils/bets/types.ts
+++ src/app/lib/utils/bets/types.ts
@@ -143,13 +143,6 @@ export type SignatureResponse = {
   signature: string;
 };
 
-export type PoolAccount = {
-  title: string;
-  hasConcluded: boolean;
-  winningOption: string;
-  value: number;
-};
-
 export type BetType = "binary" | "closest-guess-wins";
 
 export interface BetComponentProps {
