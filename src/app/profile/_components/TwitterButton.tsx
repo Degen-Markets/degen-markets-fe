794521da997766ffeac1a4fd590a431209e7af44
Angad Singh <angadsingh@Angads-Air.Home>
2024-09-03 18:30:35 +0100

    Save user's twitter profile

--- src/app/profile/_components/TwitterButton.tsx
+++ src/app/profile/_components/TwitterButton.tsx
@@ -1,17 +1,38 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { getTwitterLoginLink } from "@/app/lib/utils/api/getTwitterLoginLink";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  getTwitterLoginLink,
+  saveTwitterUser,
+} from "@/app/lib/utils/api/twitter";
 import { Button } from "@/app/components/Button";
+import { useEffect, useState } from "react";
 
 const TwitterButton = () => {
+  const [connected, setConnected] = useState(false);
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   const handleLogin = async () => {
     const linkResponse = await getTwitterLoginLink();
     router.push(linkResponse.data.url);
   };
+
+  const saveUser = async () => {
+    const twitterCode = searchParams.get("code");
+    if (twitterCode) {
+      setConnected(true);
+      const twitterUser = await saveTwitterUser(twitterCode);
+      console.log(twitterUser);
+    }
+  };
+
+  useEffect(() => {
+    saveUser();
+  }, []);
+
   return (
     <Button size={"small"} onClick={handleLogin}>
-      Connect X
+      {connected ? "Loading" : "Connect X"}
     </Button>
   );
 };
