1787a16b2b831b4591d8532bdc33343e9870496b
qalbehabib <rana.habib313@gmail.com>
2024-09-09 20:18:22 +0500

    Feat(Signature): wallet added to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Wrapper from "@/app/components/Wrapper";
+import SolanaWallet from "./SolanaWallet";
 
 const Header: React.FC = () => {
   const [nav] = useState<boolean>(false);
@@ -24,13 +25,15 @@ const Header: React.FC = () => {
       <Wrapper>
         <div className="flex justify-between items-center w-full lg:h-16">
           <div className="flex-row md:flex-col justify-center items-center flex lg:flex-row lg:justify-start lg:items-center w-full h-full py-0.5 lg:py-0">
-            <div className="flex items-center md:justify-around w-full lg:w-auto">
+            <div className="flex items-center md:justify-between w-full">
               <Link
                 href="/"
                 className="uppercase font-bold whitespace-nowrap text-3xl xl:text-4xl flex justify-center items-center text-purple-light"
               >
                 Degen Markets
               </Link>
+
+              <SolanaWallet />
             </div>
           </div>
         </div>
