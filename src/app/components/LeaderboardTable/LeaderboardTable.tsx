9a7aa0d96ccfbd541595210024c0bf3bcb6891aa
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-09-11 16:59:33 +0100

    Refactor RankIcon

--- src/app/components/LeaderboardTable/LeaderboardTable.tsx
+++ src/app/components/LeaderboardTable/LeaderboardTable.tsx
@@ -23,7 +23,7 @@ const PlayerRow: FC<PlayerRowProps> = ({
     <div className="relative">
       <UserAvatar
         src={twitterPfpUrl || "/user-avatars/default.jpg"}
-        className="rounded-lg h-12 w-12 lg:w-20 lg:h-20"
+        className="rounded-md h-16 w-16 lg:w-20 lg:h-20"
         width={90}
         height={90}
       />
@@ -36,9 +36,9 @@ const PlayerRow: FC<PlayerRowProps> = ({
         <RankIcon width={80} height={80} order={order} />
       </div>
     </div>
-    <div className="flex flex-col gap-1 justify-center">
-      <div className="text-lg font-bold">{twitterUsername}</div>
-      <span className="text-gray-400 text-lg">{points} Points</span>
+    <div className="flex flex-col gap-1 justify-center font-bold">
+      <div className="text-base">{twitterUsername}</div>
+      <span className="text-gray-400 text-sm">{points} Points</span>
     </div>
   </div>
 );
@@ -48,8 +48,8 @@ const LeaderboardTable: FC<LeaderboardProps> = ({ players }) => {
   const rightColumnPlayers = players.filter((_, index) => index % 2 !== 0);
 
   return (
-    <div className="grid grid-cols-2 gap-4 lg:gap-y-8 gap-x-16">
-      <div className="flex flex-col gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-y-8 gap-x-16">
+      <div className="flex flex-col gap-6">
         {leftColumnPlayers.map((player, index) => (
           <PlayerRow
             player={player}
@@ -59,7 +59,7 @@ const LeaderboardTable: FC<LeaderboardProps> = ({ players }) => {
           />
         ))}
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-6">
         {rightColumnPlayers.map((player, index) => (
           <PlayerRow
             player={player}
