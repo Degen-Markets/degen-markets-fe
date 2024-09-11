0a33f004bb65af770de00653a98ba3874878fa0f
Angad Singh <angadsingh@Angads-Air.Home>
2024-09-11 15:22:43 +0100

    Remove USD amount from hero section

--- src/app/components/Landing/HeroSection.tsx
+++ src/app/components/Landing/HeroSection.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => (
     </h1>
     <p className="w-full max-w-4xl mx-auto font-semibold text-lg  md:text-xl z-10">
       Make Predictions on Narrative-Driven Events Directly via Twitter. Predict
-      Now to Claim YOUR Share of the $306,000 $DGM AirDrop
+      Now to Claim YOUR Share of the $DGM AirDrop
     </p>
   </section>
 );
