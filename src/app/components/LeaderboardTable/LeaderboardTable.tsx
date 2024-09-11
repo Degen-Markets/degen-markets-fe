079bad08948ce4c151fc0c6ebb2c23d894c3da43
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-09-11 17:08:04 +0100

    Refactor LeaderboardTable cmp && remove unused cmp

--- src/app/components/LeaderboardTable/LeaderboardTable.tsx
+++ src/app/components/LeaderboardTable/LeaderboardTable.tsx
@@ -4,72 +4,76 @@ import UserAvatar from "@/app/components/UserAvatar";
 import RankIcon from "@/app/components/Icons/RankIcon";
 import { twMerge } from "tailwind-merge";
 
-interface LeaderboardProps {
-  players: Array<Player>;
-}
-
-interface PlayerRowProps extends Player {
+interface PlayerRowProps {
   player: Player;
   order: number;
 }
 
 const PlayerRow: FC<PlayerRowProps> = ({
-  twitterUsername,
-  twitterPfpUrl,
-  points,
+  player: { twitterUsername, twitterPfpUrl, points },
   order,
-}) => (
-  <div className="flex gap-4">
-    <div className="relative">
-      <UserAvatar
-        src={twitterPfpUrl || "/user-avatars/default.jpg"}
-        className="rounded-md h-16 w-16 lg:w-20 lg:h-20"
-        width={90}
-        height={90}
-      />
-      <div
-        className={twMerge(
-          "absolute",
-          order > 3 ? "-top-5 -right-5" : "-top-10 -right-10",
-        )}
-      >
-        <RankIcon width={80} height={80} order={order} />
-      </div>
-    </div>
-    <div className="flex flex-col gap-1 justify-center font-bold">
-      <div className="text-base">{twitterUsername}</div>
-      <span className="text-gray-400 text-sm">{points} Points</span>
-    </div>
-  </div>
-);
-
-const LeaderboardTable: FC<LeaderboardProps> = ({ players }) => {
-  const leftColumnPlayers = players.filter((_, index) => index % 2 === 0);
-  const rightColumnPlayers = players.filter((_, index) => index % 2 !== 0);
+}) => {
+  const isHighRank = order <= 3;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-y-8 gap-x-16">
-      <div className="flex flex-col gap-6">
-        {leftColumnPlayers.map((player, index) => (
-          <PlayerRow
-            player={player}
-            {...player}
-            order={index + 1}
-            key={index}
-          />
-        ))}
+    <div className="flex gap-4">
+      <div className="relative">
+        <UserAvatar
+          src={twitterPfpUrl || "/user-avatars/default.jpg"}
+          className="rounded-md h-16 w-16 lg:w-20 lg:h-20"
+          width={90}
+          height={90}
+        />
+        <div
+          className={twMerge(
+            "absolute",
+            isHighRank ? "-top-10 -right-10" : "-top-5 -right-5",
+          )}
+        >
+          <RankIcon width={80} height={80} order={order} />
+        </div>
       </div>
-      <div className="flex flex-col gap-6">
-        {rightColumnPlayers.map((player, index) => (
-          <PlayerRow
-            player={player}
-            {...player}
-            order={index + 1 + leftColumnPlayers.length}
-            key={index}
-          />
-        ))}
+      <div className="flex flex-col gap-1 justify-center font-bold">
+        <div className="text-base">{twitterUsername}</div>
+        <span className="text-gray-400 text-sm">{points} Points</span>
       </div>
     </div>
   );
 };
+
+interface LeaderboardProps {
+  players: Array<Player>;
+}
+
+const splitPlayersIntoColumns = (players: Array<Player>) => {
+  const leftColumnPlayers = players.filter((_, index) => index % 2 === 0);
+  const rightColumnPlayers = players.filter((_, index) => index % 2 !== 0);
+  return { leftColumnPlayers, rightColumnPlayers };
+};
+
+const LeaderboardTable: FC<LeaderboardProps> = ({ players }) => {
+  const { leftColumnPlayers, rightColumnPlayers } =
+    splitPlayersIntoColumns(players);
+
+  const renderPlayerRows = (players: Array<Player>, offset: number) =>
+    players.map((player, index) => (
+      <PlayerRow
+        player={player}
+        order={index + 1 + offset}
+        key={player.twitterUsername}
+      />
+    ));
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-y-8 gap-x-16">
+      <div className="flex flex-col gap-6">
+        {renderPlayerRows(leftColumnPlayers, 0)}
+      </div>
+      <div className="flex flex-col gap-6">
+        {renderPlayerRows(rightColumnPlayers, leftColumnPlayers.length)}
+      </div>
+    </div>
+  );
+};
+
 export default LeaderboardTable;
