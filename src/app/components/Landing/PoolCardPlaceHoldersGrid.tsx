5da1cfe48c20009516a0ccf4ca4fe837384302b2
sayandcode <sayandcode@gmail.com>
2024-09-12 18:37:55 +0530

    feat: Update pool placeholder cards to show "Create Pool" button

---
+++ src/app/components/Landing/PoolCardPlaceHoldersGrid.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "../Button/Button";
+
+const PoolPlaceholderCard = () => (
+  <div className="group relative w-full h-60 bg-blue-light rounded-xl center-all text-center p-4 bg-opacity-50 hover:scale-105 hover:shadow transition-all ease duration-300">
+    <div className="absolute inset-0 bg-black-main bg-opacity-0 group-hover:bg-opacity-70 rounded-xl transition-all ease duration-300" />
+    <div className="z-10">
+      <span className="text-5xl text-cadet-blue-light font-bold group-hover:opacity-0 transition-all ease duration-300">
+        +
+      </span>
+      <Button
+        size="regular"
+        className="!h-auto bg-opacity-90 absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-all ease duration-300"
+      >
+        Create Pool
+      </Button>
+    </div>
+  </div>
+);
+
+const PoolCardPlaceHoldersGrid = ({ count }: { count: number }) => {
+  return (
+    <>
+      {Array.from({ length: count }).map((_, ind) => (
+        <PoolPlaceholderCard key={ind} />
+      ))}
+    </>
+  );
+};
+
+export default PoolCardPlaceHoldersGrid;
