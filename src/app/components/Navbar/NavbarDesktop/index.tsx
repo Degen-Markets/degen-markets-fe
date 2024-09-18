1d199e79039cd5f806c5d4a66f4d535cb8715a55
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-09-18 16:15:59 +0100

    Use protected flag to hide profile link from the  Navbar

--- src/app/components/Navbar/NavbarDesktop/index.tsx
+++ src/app/components/Navbar/NavbarDesktop/index.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 import NavigationRoutes from "@/app/lib/utils/NavigationRoutes";
 import NavbarItem from "./NavbarItems";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export const NavbarDesktop: FC = () => {
+  const wallet = useWallet();
   return (
     <div className="hidden md:flex items-center md:justify-center lg:justify-between p-3">
       <ul className="flex items-center space-x-10">
-        {NavigationRoutes.header.navbar.map((route) => (
-          <NavbarItem key={route.name} route={route} />
-        ))}
+        {NavigationRoutes.header.navbar.map((route) => {
+          if (route.protected && !wallet.connected) return null;
+          return <NavbarItem key={route.name} route={route} />;
+        })}
       </ul>
     </div>
   );
--- src/app/components/Navbar/NavbarMobile/index.tsx
+++ src/app/components/Navbar/NavbarMobile/index.tsx
@@ -4,11 +4,13 @@ import SolanaWallet from "@/app/components/SolanaWallet";
 import { Dispatch, FC, SetStateAction } from "react";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 export const NavbarMobile: FC<{
   nav: boolean;
   setNav: Dispatch<SetStateAction<boolean>>;
 }> = ({ nav, setNav }) => {
+  const wallet = useWallet();
   return (
     <aside className="top-0 left-0 right-0 bottom-0fixed">
       <div
@@ -18,13 +20,16 @@ export const NavbarMobile: FC<{
       <div className="fixed bottom-0 right-0 h-full w-[85%] bg-white p-4 ">
         <div className="flex flex-col h-full ">
           <ul className="mt-20">
-            {NavigationRoutes.header.navbar.map((route) => (
-              <NavbarMobileItem
-                key={route.name}
-                route={route}
-                setNav={setNav}
-              />
-            ))}
+            {NavigationRoutes.header.navbar.map((route) => {
+              if (route.protected && !wallet.connected) return null;
+              return (
+                <NavbarMobileItem
+                  key={route.name}
+                  route={route}
+                  setNav={setNav}
+                />
+              );
+            })}
           </ul>
           <div className="flex justify-center cursor-pointer uppercase py-4 tracking-wider text-base mt-20">
             <SolanaWallet />
