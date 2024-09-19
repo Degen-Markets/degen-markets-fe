dbf70ffc6e087779d121222ad23acb94f2797b74
sayandcode <sayandcode@gmail.com>
2024-09-19 18:20:32 +0530

    feat: Add `claimPoolTweetPoints` API util
    This will help us claim points for a user's tweet. Also the other twitter
    utils were made to use `API_BASE_URL` as well, instead of harcoding the
    base URL.

--- src/app/lib/utils/api/twitter.ts
+++ src/app/lib/utils/api/twitter.ts
@@ -1,16 +1,32 @@
 import { Player } from "@/app/types/player";
 import axios, { AxiosResponse } from "axios";
+import { API_BASE_URL } from ".";
 
 export const getTwitterLoginLink = () =>
-  axios.get<{ url: string }>("https://api.degenmarkets.com/twitter-login");
+  axios.get<{ url: string }>(`${API_BASE_URL}/twitter-login`);
 
 export const saveTwitterProfile = (
   twitterCode: string,
   signature: string,
   address: string,
 ): Promise<AxiosResponse<Player>> =>
-  axios.post("https://api.degenmarkets.com/save-twitter-profile", {
+  axios.post(`${API_BASE_URL}/save-twitter-profile`, {
     twitterCode,
     signature,
     address,
+  });
+
+export const claimPoolTweetPoints = ({
+  tweetUrl,
+  poolId,
+  playerAddress,
+}: {
+  tweetUrl: string;
+  poolId: string;
+  playerAddress: string;
+}): Promise<AxiosResponse<{ message: string; pointsAwarded: number }>> =>
+  axios.post(`${API_BASE_URL}/claim-pool-tweet-points`, {
+    poolId,
+    tweetUrl,
+    playerAddress,
   });
