c995f02688ff378b6f139281e04cdd6598637f75
sayandcode <sayandcode@gmail.com>
2024-09-23 11:38:42 +0530

    feat: Update SolanaWallet button design
    The new design is more rounded (like our old website), and includes the
    user's pfp as well. We added the `UserProfileProvider` to the `SolanaWallet`
    component in order to allow this

--- src/app/components/SolanaWallet.tsx
+++ src/app/components/SolanaWallet.tsx
@@ -1,16 +1,69 @@
 "use client";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Image from "next/image";
 import { FC } from "react";
+import {
+  UserProfileProvider,
+  useUserProfileContext,
+} from "../context/UserProfileContext";
+import UserAvatar from "./UserAvatar";
+import { useWallet } from "@solana/wallet-adapter-react";
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
-const SolanaWallet: FC = () => {
+const SolanaWallet = () => {
+  return (
+    <UserProfileProvider>
+      <Content />
+    </UserProfileProvider>
+  );
+};
+
+const Content: FC = () => {
+  const { userProfile } = useUserProfileContext();
+  const { publicKey, connected, connecting } = useWallet();
+  const publicKeyString = publicKey?.toBase58();
+  const walletAddrDisplayStr = publicKeyString
+    ? `${publicKeyString.slice(0, 4)}...${publicKeyString.slice(-4)}`
+    : null;
+
   return (
     <WalletMultiButton
-      style={{ textWrap: "nowrap", background: "#AB9FF2" }}
+      style={{
+        textWrap: "nowrap",
+        background: "#AB9FF2",
+        borderRadius: "2rem",
+        fontFamily: "inherit",
+      }}
       className="!w-full"
-    />
+    >
+      <div className="flex items-center gap-2">
+        {connected && walletAddrDisplayStr ? (
+          <>
+            <UserAvatar
+              src={userProfile.twitterPfpUrl}
+              className="rounded-full h-12 w-12 absolute top-0 left-0 "
+              width={90}
+              height={90}
+            />
+            {walletAddrDisplayStr}
+          </>
+        ) : (
+          <>
+            {connecting ? null : (
+              <Image
+                src={"/navIcons/Wallet.svg"}
+                alt="Wallet"
+                width={24}
+                height={24}
+              />
+            )}
+            Connect Wallet
+          </>
+        )}
+      </div>
+    </WalletMultiButton>
   );
 };
 
