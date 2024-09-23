eaa11737645f2900849b33d82849bd31e0a41695
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-09-23 10:38:27 +0100

    Adjust text size for profile page

--- src/app/my-profile/_component/UserProfileInfo.tsx
+++ src/app/my-profile/_component/UserProfileInfo.tsx
@@ -34,18 +34,18 @@ const UserProfileInfo: React.FC = () => {
           </div>
         </div>
         <div className="space-y-6 font-bold">
-          <h2 className="text-3xl  text-center md:text-left md:text-5xl">
+          <h2 className="text-xl text-center md:text-left md:text-3xl">
             {userProfile?.twitterUsername
               ? `@${userProfile.twitterUsername}`
               : "@Degen"}
           </h2>
           <div className="flex items-center space-x-4">
-            <p className="text-gray-400 rounded-full bg-black-light py-2 px-4 text-center">
+            <p className="text-gray-400 rounded-full bg-black-light py-2 px-4 text-center text-base">
               {address
                 ? getDisplayNameForAddress(address as Address)
                 : "0XXX...XXX"}
             </p>
-            <p className="text-gray-400">Joined Oct 2024</p>
+            <p className="text-gray-400 text-base">Joined Oct 2024</p>
           </div>
         </div>
       </div>
