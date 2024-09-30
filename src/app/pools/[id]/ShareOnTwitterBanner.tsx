4c474ef17438e69148c0868e6bb57f1c0e274ccb
sayandcode <sayandcode@gmail.com>
2024-09-30 17:04:05 +0530

    refactor: Move `useShareOnTwitterFlow` to a separate util file

--- src/app/pools/[id]/ShareOnTwitterBanner.tsx
+++ src/app/pools/[id]/ShareOnTwitterBanner.tsx
@@ -11,6 +11,7 @@ import ConnectTwitterDialog from "./ConnectTwitterDialog";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useConnectProfileThen } from "@/app/hooks/enhancedWallet";
 import { Pool } from "@/app/lib/utils/types";
+import useShareOnTwitterFlow from "./utils/useShareOnTwitterFlow";
 
 export const RESUME_SHARE_FLOW_SEARCH_PARAM = {
   key: "resume-share",
@@ -65,70 +66,6 @@ const Content = ({ poolId }: { poolId: Pool["address"] }) => {
       />
     </div>
   );
-};
-
-const useShareOnTwitterFlow = ({
-  poolId,
-  openClaimDialog,
-  openConnectTwitterDialog,
-}: {
-  poolId: Pool["address"];
-  openClaimDialog: () => void;
-  openConnectTwitterDialog: () => void;
-}) => {
-  const { connected } = useWallet();
-  const connectProfileThen = useConnectProfileThen();
-  const searchParams = useSearchParams();
-  const { userProfile, isProfileLoading } = useUserProfileContext();
-
-  const startShareFlow = useCallback(() => {
-    const tweetText = encodeURIComponent(
-      `Place your bets on @DEGEN_MARKETS\ndegenmarkets.com/pools/${poolId}`,
-    );
-    window.open(`https://x.com/compose/post?text=${tweetText}`, "_blank");
-    openClaimDialog();
-  }, [poolId, openClaimDialog]);
-
-  const isReadyToShare =
-    connected && !isProfileLoading && !!userProfile?.twitterUsername;
-
-  const handleShare = useCallback(() => {
-    if (!connected) {
-      connectProfileThen(async (userProfile) => {
-        if (!!userProfile?.twitterUsername) {
-          startShareFlow();
-        } else {
-          openConnectTwitterDialog();
-        }
-      });
-      return;
-    }
-    if (!isReadyToShare) {
-      openConnectTwitterDialog();
-      return;
-    }
-    startShareFlow();
-  }, [
-    openConnectTwitterDialog,
-    isReadyToShare,
-    connectProfileThen,
-    connected,
-    startShareFlow,
-  ]);
-
-  const isResumingShareViaSearchParam =
-    searchParams.get(RESUME_SHARE_FLOW_SEARCH_PARAM.key) ===
-    RESUME_SHARE_FLOW_SEARCH_PARAM.values.true;
-  const isReadyToResumeShare = isResumingShareViaSearchParam && isReadyToShare;
-  useEffect(() => {
-    if (!isReadyToResumeShare) {
-      return;
-    }
-
-    startShareFlow();
-  }, [isReadyToResumeShare, startShareFlow]);
-
-  return { handleShare };
 };
 
 export default ShareOnTwitterBanner;
