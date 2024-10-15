ca7331a6ebbfe299bfc9f9815f86a0c13b92379c
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-10-15 17:51:57 +0100

    Adjust style for pool page

--- src/app/pools/[id]/page.tsx
+++ src/app/pools/[id]/page.tsx
@@ -11,7 +11,7 @@ const PoolPage = async ({ params: { id } }: { params: { id: string } }) => {
 
   return (
     <Wrapper className="flex justify-center">
-      <div className="w-full md:w-3/6 lg:w-2/6 text-lg">
+      <div className="w-full md:w-3/6 lg:w-2/6 text-lg mt-4 lg:mt-8">
         <ShareOnTwitterBanner poolId={id} />
         <BlinkLoader poolId={id} poolValue={value} />
       </div>
