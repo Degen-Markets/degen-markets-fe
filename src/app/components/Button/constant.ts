2ab269f6e09a5eaad046b6cb903be7bc9db3a7fb
qalbehabib <rana.habib313@gmail.com>
2024-10-21 16:31:48 +0500

    Fix(size): button resized uniformed.

--- src/app/components/Button/constant.ts
+++ src/app/components/Button/constant.ts
@@ -5,19 +5,19 @@ export const buttonVariants = cva(
   {
     variants: {
       intent: {
-        main: "bg-primary hover:bg-primary-light active:bg-primary-light text-main",
+        main: "bg-primary border-2 border-primary hover:border-primary-light hover:bg-primary-light active:bg-primary-light text-main",
         primary:
-          "bg-primary hover:bg-primary-light active:bg-primary-light text-main",
+          "bg-primary hover:bg-primary-light active:bg-primary-light text-main border-2 border-primary hover:border-primary-light",
         outlineWhite:
           "bg-transparent border-2 border-white hover:bg-white active:bg-white hover:text-main active:text-main",
         outline:
           "bg-transparent border-2 border-primary hover:bg-primary active:bg-primary hover:text-main active:text-main text-primary active:text-main",
         secondary:
-          "bg-secondary hover:bg-opacity-80 active:bg-opacity-80 text-main",
+          "bg-secondary border-2 border-secondary hover:border-secondary-light hover:bg-secondary-light active:bg-opacity-80 text-main",
         danger:
-          "bg-danger text-main hover:bg-danger-light active:bg-danger-light",
+          "bg-danger border-2 border-danger text-main hover:bg-danger-light active:bg-danger-light",
         success:
-          "bg-success text-main hover:bg-bg-success-light active:bg-success-light",
+          "bg-success border-2 border-success text-main hover:bg-bg-success-light active:bg-success-light",
         transparent: "hover:bg-neutral-950/10 active:bg-neutral-950/10",
       },
       size: {
