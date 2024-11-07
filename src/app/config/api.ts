9abb22c998578390c78f205da1809f63ee8a10ce
qalbehabib <rana.habib313@gmail.com>
2024-11-07 23:49:18 +0500

    Fix(create-bet): fixing dev create bet

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -2,3 +2,13 @@ export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.degenmarkets.com";
 
 export const FREENAME_API_BASE_URL = "https://apis.freename.io/api/v1";
+
+export const HELIUS_RPC_URL =
+  process.env.NEXT_PUBLIC_HELIUS_URL ||
+  "https://mainnet.helius-rpc.com/?api-key=e56ebb46-0709-4b0c-907e-4b6aa24d281b";
+
+export const ACTION_API_URL =
+  process.env.NEXT_PUBLIC_ACTION_API_URL || "https://degenmarkets.com";
+
+export const ACTION_API_PATH =
+  process.env.NEXT_PUBLIC_ACTION_API_PATH || "https://actions.degenmarkets.com";
