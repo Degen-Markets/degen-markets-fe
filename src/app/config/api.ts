6ed294647215640ef0e24f06373f1b73e15fb515
qalbehabib <rana.habib313@gmail.com>
2024-11-08 11:58:19 +0500

    Fix(config) hardcoded config added for dev and prod environment

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,14 +1,8 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.degenmarkets.com";
+import Config from "./config";
+
+export const API_BASE_URL = Config.API_BASE_URL;
+export const HELIUS_RPC_URL = Config.HELIUS_RPC_URL;
+export const ACTION_API_URL = Config.ACTION_API_URL;
+export const ACTION_API_PATH = Config.ACTION_API_PATH;
 
 export const FREENAME_API_BASE_URL = "https://apis.freename.io/api/v1";
-
-export const HELIUS_RPC_URL =
-  process.env.NEXT_PUBLIC_HELIUS_URL ||
-  "https://mainnet.helius-rpc.com/?api-key=e56ebb46-0709-4b0c-907e-4b6aa24d281b";
-
-export const ACTION_API_URL =
-  process.env.NEXT_PUBLIC_ACTION_API_URL || "https://degenmarkets.com";
-
-export const ACTION_API_PATH =
-  process.env.NEXT_PUBLIC_ACTION_API_PATH || "https://actions.degenmarkets.com";
