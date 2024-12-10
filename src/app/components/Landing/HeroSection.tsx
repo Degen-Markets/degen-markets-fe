b45a5eeb325f93b87a4ebbaa51d1ed9218d428f1
angadsingh <angad17@hotmail.com>
2024-12-10 12:28:51 +0000

    add pb-20 to HeroSection

--- src/app/components/Landing/HeroSection.tsx
+++ src/app/components/Landing/HeroSection.tsx
@@ -7,33 +7,33 @@ import { Section } from "@/app/components/Section";
 import RobotIcon from "@/app/components/Icons/RobotIcon";
 
 const HeroSection = () => (
-  <div className="bg-hero">  
-     <Section className="space-y-5 lg:space-y-10 flex gap-4 ">
-
-    <>
-      <div className="w-full lg:w-[60%]">
-        <h1 className="text-2xl md:text-6xl md:leading-snug  mb-8">
-          Dare to disagree with the prophecies?
-        </h1>
-        <p className="w-[80%] lg:w-[60%] text-base md:text-md text-lavender-blue mb-6">
-          Go head to head against AI predictions
-        </p>
-        <Link href="/pools">
-          <Button intent="primary" icon={<RobotIcon width={30} />}>
-            Bet Against AI
-          </Button>
-        </Link>
-      </div>
-      <div className="hidden lg:block relative w-[40%]">
-        <div className="absolute top-5 right-0">
-          <DotLottieReact src="/animations/hero-shape.lottie" loop autoplay />
+  <div className="bg-hero pb-20">
+    <Section className="space-y-5 lg:space-y-10 flex gap-4 ">
+      <>
+        <div className="w-full lg:w-[60%]">
+          <h1 className="text-2xl md:text-6xl md:leading-snug  mb-8">
+            Dare to disagree with the prophecies?
+          </h1>
+          <p className="w-[80%] lg:w-[60%] text-base md:text-md text-lavender-blue mb-6">
+            Go head to head against AI predictions
+          </p>
+          <Link href="/pools">
+            <Button intent="primary" icon={<RobotIcon width={30} />}>
+              Bet Against AI
+            </Button>
+          </Link>
         </div>
-        <div className="absolute">
-          <DotLottieReact src="/animations/blink.lottie" autoplay />
+        <div className="hidden lg:block relative w-[40%]">
+          <div className="absolute top-5 right-0">
+            <DotLottieReact src="/animations/hero-shape.lottie" loop autoplay />
+          </div>
+          <div className="absolute">
+            <DotLottieReact src="/animations/blink.lottie" autoplay />
+          </div>
         </div>
-      </div>
-    </>
-  </Section>
+      </>
+    </Section>
+  </div>
 );
 
 export default HeroSection;
